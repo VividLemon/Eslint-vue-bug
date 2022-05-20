2bf186fee840a27df62bef08a955287452e2ad96
issayah <kwiksilver344@gmail.com>
2022-05-20 15:16:21 -0500

    init

--- src/views/Third.vue
+++ src/views/Third.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script setup lang="ts">
-// defineComponent is never read
+// defineComponent is never read, but error is gone. Not sure how this would behave in build
 import { ref, defineComponent } from 'vue'
 
 const a = ref(1)
